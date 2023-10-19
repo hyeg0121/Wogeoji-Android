f7e49e00443622c6e249adc050ba76178db95bc5
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-19 19:38:03 +0900

    layout

---
+++ app/src/main/java/com/example/wogeoji/activity/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.example.wogeojiapp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.wogeojiapp.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        supportActionBar?.hide()
+    }
+}
