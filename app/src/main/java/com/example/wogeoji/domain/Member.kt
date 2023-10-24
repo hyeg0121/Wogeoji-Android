fc8ae9ad817ab1ab74d247f1917958e6567a78c2
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-24 19:42:00 +0900

    feat: member dto 수정

--- app/src/main/java/com/example/wogeoji/domain/Member.kt
+++ app/src/main/java/com/example/wogeoji/domain/Member.kt
@@ -1,8 +1,10 @@
 package com.example.wogeoji.domain
 
 data class Member(
+    val memberNo: Long,
     val name: String,
-    val id: String,
+    val memberId: String,
     val password: String,
-    val email: String
+    val email: String,
+    val groups: Set<Group>
 )
