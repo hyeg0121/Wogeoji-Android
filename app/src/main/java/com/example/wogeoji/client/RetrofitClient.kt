bef1518be5b9eb8a17fad474b8ebaa5bbb54ed78
hyeg0121 <w2218@e-mirim.hs.kr>
2024-03-07 10:55:41 +0900

    feat: 레트로핏 클라이언트 구현

--- app/src/main/java/com/example/wogeoji/client/RetrofitClient.kt
+++ app/src/main/java/com/example/wogeoji/client/RetrofitClient.kt
@@ -3,7 +3,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
     private var retrofit: Retrofit? = null
-    private const val BASE_URL = "http://10.0.2.2:8080" // API의 기본 URL을 여기에 넣으세요
+    private const val BASE_URL = "http://10.0.2.2:8080"
 
     fun getRetrofitInstance(): Retrofit {
         if (retrofit == null) {
