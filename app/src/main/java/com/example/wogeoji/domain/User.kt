63ef0f4d003e0f167b6668b19a3155faa6835265
hyeg0121 <w2218@e-mirim.hs.kr>
2024-03-12 09:31:41 +0900

    feat: User 데이터 클래스 타입 변경

--- app/src/main/java/com/example/wogeoji/domain/User.kt
+++ app/src/main/java/com/example/wogeoji/domain/User.kt
@@ -1,16 +1,17 @@
 package com.example.wogeoji.domain
 
+import java.time.LocalDateTime
 import java.util.Date
 
 data class User(
+    val isDeleted: Int,
+    val createdAt: String,
+    val updatedAt: String,
     val id: Long,
     val name: String,
     val email: String,
     val password: String,
-    val bio: String?,
-    val profileImagePath: String?,
-    val spentAmount: Int,
-    val isDelted: Int,
-    val createdAt: Date,
-    val updatedAt: Date,
+    val bio: String,
+    val profileImagePath: String,
+    val spentAmount: Int
 )
