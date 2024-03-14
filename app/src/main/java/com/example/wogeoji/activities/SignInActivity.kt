c8ced470a906c35a98097f1580ef222db38fdd26
hyeg0121 <w2218@e-mirim.hs.kr>
2024-03-14 22:00:24 +0900

    feat: 로그인 기능 구현

--- app/src/main/java/com/example/wogeoji/activities/SignInActivity.kt
+++ app/src/main/java/com/example/wogeoji/activities/SignInActivity.kt
@@ -2,42 +2,55 @@ package com.example.wogeoji.activities
 
 import RetrofitClient
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.example.wogeoji.MainActivity
 import com.example.wogeoji.R
 import com.example.wogeoji.domain.User
 import com.example.wogeoji.dto.user.SignInRequest
+import com.example.wogeoji.util.ToastHelper
 import com.example.wogeojiapp.service.UserService
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class SignInActivity : AppCompatActivity() {
+
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var signInButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_sign_in)
 
-        val sharedPreferences = getSharedPreferences("myPref", Context.MODE_PRIVATE)
-        val editor = sharedPreferences.edit()
+        emailEditText = findViewById(R.id.email_edittext)
+        passwordEditText = findViewById(R.id.pw_edittext)
+        signInButton = findViewById(R.id.login_button)
 
-        val emailEditText = findViewById<EditText>(R.id.email_edittext)
-        val pwEditText = findViewById<EditText>(R.id.pw_edittext)
-        val loginButton = findViewById<Button>(R.id.login_button)
-
-        loginButton.setOnClickListener {
+        signInButton.setOnClickListener {
+            Log.d("my tag", "click")
             val email = emailEditText.text.toString().trim()
-            val pw = pwEditText.text.toString().trim()
+            val password = passwordEditText.text.toString().trim()
 
-            Log.d("my tag", SignInRequest(email, pw).toString())
-            performLogin(SignInRequest(email, pw))
+
+            // 이메일 및 비밀번호 유효성 검사
+            if (email.isEmpty() || password.isEmpty()) {
+                ToastHelper.showToast(this@SignInActivity, "이메일 또는 비밀번호를 입력해주세요.")
+            } else {
+
+                signInUser(SignInRequest(email, password))
+            }
+
         }
 
     }
 
-    fun performLogin(loginRequest: SignInRequest) {
+    private fun signInUser(loginRequest: SignInRequest) {
         val retrofit = RetrofitClient.getRetrofitInstance()
         val userService = retrofit.create(UserService::class.java)
         val call = userService.loginUser(loginRequest)
@@ -46,15 +59,21 @@ class SignInActivity : AppCompatActivity() {
             override fun onResponse(call: Call<User>, response: Response<User>) {
                 if (response.isSuccessful) {
                     val user = response.body()
-                    Log.d("my tag", "user" + response.toString())
+                    Log.d("my tag", "user" + user.toString())
+                    navigateToMainActivity()
                 }
             }
 
             override fun onFailure(call: Call<User>, t: Throwable) {
-                // TODO: 실패 했을떄 토스트 메세지
-                Log.d("my tag", "error" + t.toString())
+                ToastHelper.showToast(this@SignInActivity, "로그인에 실패하였습니다.")
             }
 
         })
+    }
+
+    private fun navigateToMainActivity() {
+        val intent = Intent(this@SignInActivity, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
